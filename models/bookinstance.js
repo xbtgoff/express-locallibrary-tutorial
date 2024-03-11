1010799cc3461a51c95807e9b6b52a02cf56ab2a
Денис <denispusiack@gmail.com>
2024-03-11 11:28:57 +0200

    Add dynamic content to books and book-instances pages

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
 
 const Schema = mongoose.Schema;
 
@@ -18,6 +19,12 @@ BookInstanceSchema.virtual("url").get(function () {
   // We don't use an arrow function as we'll need the this object
   return `/catalog/bookinstance/${this._id}`;
 });
+
+// Virtual for a better Date format
+BookInstanceSchema.virtual("due_back_formatted").get(function () {
+  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
+});
+
 
 // Export model
 module.exports = mongoose.model("BookInstance", BookInstanceSchema);
