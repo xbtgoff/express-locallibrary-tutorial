daa5a6d8cbd015adf8b7caf826b4a6acbd038554
Денис <denispusiack@gmail.com>
2024-04-08 10:44:05 +0300

    prep for deployment

--- app.js
+++ app.js
@@ -7,14 +7,38 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
-
+const compression = require("compression");
+const helmet = require("helmet");
 
 var app = express();
+
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 100,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
+
+// Add helmet to the middleware chain.
+// Set CSP headers to allow our Bootstrap and Jquery to be served
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  }),
+);
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = "mongodb+srv://admin:ah7LNmmemoKWiQta@cluster0.rupqbec.mongodb.net/local_library?retryWrites=true&w=majority&appName=Cluster0";
+//const mongoDB = "mongodb+srv://admin:ah7LNmmemoKWiQta@cluster0.rupqbec.mongodb.net/local_library?retryWrites=true&w=majority&appName=Cluster0";
+
+const dev_db_url =
+  "mongodb+srv://admin:ah7LNmmemoKWiQta@cluster0.rupqbec.mongodb.net/local_library?retryWrites=true&w=majority&appName=Cluster0";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 
 main().catch((err) => console.log(err));
@@ -30,6 +54,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression()); //Compress all routes
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
